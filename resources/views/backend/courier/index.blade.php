6363c8e135be47ef41e4371e1760c581b81e2c99
Abdullah Syed <sabexel@gmail.com>
2025-07-29 08:55:48 +0500

    Update index.blade.php

--- resources/views/backend/courier/index.blade.php
+++ resources/views/backend/courier/index.blade.php
@@ -5,6 +5,16 @@
 @if(session()->has('not_permitted'))
   <div class="alert alert-danger alert-dismissible text-center"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>{{ session()->get('not_permitted') }}</div>
 @endif
+@if($errors->any())
+    <div class="alert alert-danger">
+        <ul class="mb-0">
+            @foreach($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+
 
 <section>
     <div class="container-fluid">
